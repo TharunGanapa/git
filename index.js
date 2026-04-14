6634c1f144a1188305e366b5f7ff4408c9b16e35
Tharun Ganapa <ganapatharun@gmail.com>
2026-04-14 09:45:27 +0530

    added content

---
+++ index.js
@@ -0,0 +1,19 @@
+// let name = "riya";
+// let age = "22";
+
+// console.log("My ... name is " + name +". she is " + age);
+// console.log(`My ... name is ${name} . she is ${age}`);
+
+// let buchi = document.getElementById("buchi");
+
+// buchi.innerText = "<b>hello</b>";
+// buchi.innerText = "<b>hello</b>";
+
+
+let boxes = document.getElementsByClassName("box");
+
+let para = document.getElementsByClassName("para");
+
+boxes[1].innerHTML = "bye";
+
+para[0].innerHTML = "holaa";
