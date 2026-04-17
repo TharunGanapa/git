7c725fb10447b15afe38d839bfadbf0ab54922d9
Tharun Ganapa <ganapatharun@gmail.com>
2026-04-17 09:15:34 +0530

    added content

--- index.js
+++ index.js
@@ -37,15 +37,30 @@
     
 // };
 
-const add = () => {
-    event.preventDefault();
-    let val1 = Number(document.getElementById("value1").value)
-    let val2 = Number(document.getElementById("value2").value)
-    let display = document.getElementById("display")
+// const add = () => {
+//     event.preventDefault();
+//     let val1 = Number(document.getElementById("value1").value)
+//     let val2 = Number(document.getElementById("value2").value)
+//     let display = document.getElementById("display")
 
-    display.innerHTML = `sum of ${val1} and ${val2} is ${val1 + val2}`
+//     display.innerHTML = `sum of ${val1} and ${val2} is ${val1 + val2}`
+
+// }
+
+// changing color
 
 
+const  changeColor =() => {
+    let display = document.getElementById("display");
+    let color = "#" // 6
+    let colorCOdes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
 
+    for(let i=0;i<6;i++){
+        let randNo = Math.floor(Math.random() * 16)
+        color += colorCOdes[randNo]
+    }
 
+    display.style.backgroundColor = color
 }
+
+changeColor()
